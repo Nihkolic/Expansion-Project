560030adbfcb15b0160cafb25723dca0582ac8f0
EUGENESTRIX <albertoponcebalcazar@gmail.com>
2021-04-28 05:08:33 -0500

    Inventory rough prototype
    - inventory rough scripts
    - rigidbody based player start

---
+++ EXP/Assets/Scripts/PickUp.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject goItemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.goSlots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //item can be added to the inventory
+                    inventory.isFull[i] = true;
+                    Instantiate(goItemButton, inventory.goSlots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ EXP/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Inputs();
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    void Inputs()
+    {
+
+    }
+    void Movement()
+    {
+
+    }
+}
