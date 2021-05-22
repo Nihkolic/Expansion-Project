c52da36592dbce1ce4547858b79a4e913a572356
Jeanfranco <playmaxgamer@gmail.com>
2021-05-21 22:47:02 -0500

    Agreganddo script MenuModelo y MenuVista

---
+++ EXP/Assets/Scripts/Menu/MenuModelo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuModelo : MonoBehaviour
+{
+    public MenuVista vista;
+    public string escenarioIni;
+    public string escenarioCre;
+
+    void Start()
+    {
+        
+    }
+
+}
---
+++ EXP/Assets/Scripts/Menu/MenuVista.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuVista : MonoBehaviour
+{
+    public MenuModelo modelo;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
