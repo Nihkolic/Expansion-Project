4c02c973d3bd9fd9983c7d29ebbbd003341077ad
AlbertoPonce <albertoponcebalcazar@gmail.com>
2021-05-28 00:03:22 -0500

    Healing and damage effects
    plus UI health numbers

--- EXP/Assets/Scripts/Player/PlayerHealth.cs
+++ EXP/Assets/Scripts/Player/PlayerHealth.cs
@@ -6,37 +6,39 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     public int hp;
-    public int hpMax = 15;
+    public int hpMax = 20;
     public Image hpBar;
-    //public GameObject goBloodScreen;
-    public Animator animBloodScreen;
+ 
+    public Animator screenEffect;
+    public Text textHP;
+    
     private void Start()
     {
+        hpMax *= 10;
         hp = hpMax;
+        textHP.text = ("  " + hp.ToString() + " / " + hpMax.ToString());
     }
     public void Damage(int amount)
     {
-        hp -= amount;
+        hp -= amount*10;
         if (hp < 0) hp = 0;
-        animBloodScreen.Play("Hurt");
+        screenEffect.Play("HurtScreen");
         UpdateUI();
     }
     public void Heal(int amount)
     {
-        hp += amount;
+        hp += amount*10;
         if (hp > hpMax) hp = hpMax;
+        screenEffect.Play("HealScreen");
         UpdateUI();
     }
     private void UpdateUI()
     {
         hpBar.fillAmount = GetHealthPercent();
+        textHP.text = ("  " + hp.ToString() + " / " + hpMax.ToString());
     }
     public float GetHealthPercent()
     {
         return (float)hp / hpMax;
-    }
-    public void ResetHurtAnim()
-    {
-        animBloodScreen.Play("Idle");
     }
 }
