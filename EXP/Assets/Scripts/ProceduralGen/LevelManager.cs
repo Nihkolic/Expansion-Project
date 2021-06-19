e773ab5598387f281c454642373d1e2aa745db9f
AlbertoPonce <albertoponcebalcazar@gmail.com>
2021-06-18 23:01:51 -0500

    testing different procedural gen

--- EXP/Assets/Scripts/ProceduralGen/LevelManager.cs
+++ EXP/Assets/Scripts/ProceduralGen/LevelManager.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public int[] seedArray;
+    public bool randomSeed;
+    public int seed;
+    int _seed;
 
-    public Tile[] tiles;
-    public int[] seed;
+    public int gridX;
+    public int gridz;
+    float gridSpacingOffsetX = 94.47976f;
+    float gridSpacingOffsetZ = 97.80977f;
+    Vector3 gridOrigin = Vector3.zero;
 
-    private void Start()
+    public GameObject[] tiles;
+    private void Awake()
     {
-        
+        GenerateGrid();
     }
-    public void Generatelevel()
+    void GenerateSeed()
     {
-        //Vector3 lastPosition = Vector3.zero;
-
-        for (int i = 0; i < seed.Length; i++)
+        if (randomSeed)
         {
+            _seed = Random.Range(0, 9999);
+        }
+        else
+        {
+            for (int i = 0; i < seedArray.Length; i++)
+            {
+                _seed = seedArray[i];
+            }
+        }
 
+    }
+    public void GenerateGrid()
+    {
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int z = 0; z < gridz; z++)
+            {
+                Vector3 spawnPosition = new Vector3(x * gridSpacingOffsetX, 0, z * gridSpacingOffsetZ) + gridOrigin;
+                PickAndSpawn(spawnPosition);
+            }
         }
     }
+    void PickAndSpawn(Vector3 positionToSpawn)
+    {
+        int randomIndex = Random.Range(0, tiles.Length);
+        GameObject clone = Instantiate(tiles[randomIndex], positionToSpawn, Quaternion.identity);
+    }
+
     public void GenerateMap()
     {
         int[,] map = new int[3, 3]
